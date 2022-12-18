f64ca4e79bbc7c9fae7442bf65d30864aef31e56
Dave Harms <dave.harms@fortyau.com>
2022-12-18 05:06:18 -0600

    routing complete

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -8,7 +8,6 @@ function ButtonPage() {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <Button onClick={buttonHandler} primary rounded outline>
           <GoBell />
